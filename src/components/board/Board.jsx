26be3da60a60f093d775ca372343ba8c0be8a657
Tarik G <tg@Tariks-MacBook-Air.local>
2025-11-12 21:47:56 -0500

    added an idPrefix for the creatio of the second board

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import BoardFakeRow from "./BoardFakeRow";
 
-const Board = ({puzzleFragment, category}) => {
+const Board = ({ puzzleFragment, category, idPrefix = "" }) => {
   // parameters:
   // "puzzleFragment" and "category" are both strings
 
@@ -12,46 +12,57 @@ const Board = ({puzzleFragment, category}) => {
   //make the interactable rows with a loop
   //the keys determine which squares are white or green
 
+  // added a id prefix because there are 2 boards on the screen
+  // need to differentiate the 2 boards from each other
+  const prefix = idPrefix ? `${idPrefix}_` : "";
+
   let autoRows = [];
-  for(let i = 0; i < 2; i++){
+  for (let i = 0; i < 2; i++) {
     let row = [];
 
-    for(let j = 0; j < 12; j++){
+    for (let j = 0; j < 12; j++) {
       const index = i * 12 + j;
-      row.push(<div className="board-box" key={index} id={"board_square_"+index}></div>);
-    }//for
+      row.push(
+        <div
+          className="board-box"
+          key={index}
+          id={`${prefix}board_square_${index}`}
+        ></div>
+      );
+    } //for
 
     autoRows.push(row);
-  }//for
+  } //for
 
   useEffect(() => {
     //reset all squares
-    for(let i = 0; i < 24; i++){
-      const square = document.getElementById("board_square_"+i);
-      if(square){
+    for (let i = 0; i < 24; i++) {
+      const square = document.getElementById(`${prefix}board_square_${i}`);
+      if (square) {
         square.style.backgroundColor = "mediumseagreen";
         square.innerHTML = "";
       }
     }
 
     //flag the squares without spaces
-    for(let i = 0; i < puzzleFragment.length; i++){
-      if(puzzleFragment[i] !== " "){
-        const square = document.getElementById("board_square_"+i);
+    for (let i = 0; i < puzzleFragment.length; i++) {
+      if (puzzleFragment[i] !== " ") {
+        const square = document.getElementById(`${prefix}board_square_${i}`);
 
-        if(square){
+        if (square) {
           square.style.backgroundColor = "#FFFFFF";
-          square.innerHTML = (puzzleFragment[i] === "*") ? " " : puzzleFragment[i];
-        }//if
-      }//if
-    }//for
+          square.innerHTML =
+            puzzleFragment[i] === "*" ? " " : puzzleFragment[i];
+        } //if
+      } //if
+    } //for
   }, [puzzleFragment]); // Re-run when puzzleFragment changes
-  
+
   return (
     <>
       <div>
         <div id="board_grid">
-          <BoardFakeRow/>
+          <BoardFakeRow />
 
           <div className="board-grid-row">
             <div className="board-box"></div>
@@ -69,12 +80,12 @@ const Board = ({puzzleFragment, category}) => {
             <div className="board-box"></div>
           </div>
 
-          <BoardFakeRow/>
+          <BoardFakeRow />
         </div>
         <div id="board_div_category">{category}</div>
       </div>
     </>
   );
-}
+};
 
 export default Board;
